6ffa679ec650eaecbefbb6e82c88ca0d557e91b9
ajandaur <anmol.jandaur@gmail.com>
2022-02-17 19:17:54 -0800

    Initial Commit

---
+++ Cat_World/Cat_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_WorldApp.swift
+//  Cat_World
+//
+//  Created by Anmol  Jandaur on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
